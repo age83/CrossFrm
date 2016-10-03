84e8f1d82954ffd8c3ebb64f10150da5af997fb0
Andreea ERDELYI <andreea.erdelyi@cososys.com>
2016-10-03 17:18:23 +0300

    cross platform project settings + unit testing

---
+++ Sources/CrossFrm.h
@@ -0,0 +1,19 @@
+//
+//  CrossFrm.h
+//  CrossFrm
+//
+//  Created by Andreea Erdelyi on 03/10/16.
+//
+//
+
+@import Foundation;
+
+//! Project version number for CrossFrm.
+FOUNDATION_EXPORT double CrossFrmVersionNumber;
+
+//! Project version string for CrossFrm.
+FOUNDATION_EXPORT const unsigned char CrossFrmVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrossFrm/PublicHeader.h>
+
+
---
+++ Sources/Utils.swift
@@ -0,0 +1,18 @@
+//
+//  Utils.swift
+//  CrossFrm
+//
+//  Created by Andreea Erdelyi on 03/10/16.
+//
+//
+
+import Foundation
+
+public class Utils {
+
+    public static func lenght(ofText text: String) -> Int {
+
+        return text.characters.count
+    }
+
+}
