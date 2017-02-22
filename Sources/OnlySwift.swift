3826354e59940fd1b8bdfe7069f52cbc01450ab3
Andreea ERDELYI <andreea.erdelyi@cososys.com>
2017-02-22 16:18:13 +0200

    swift/objc test

---
+++ Sources/OnlySwift.swift
@@ -0,0 +1,17 @@
+//
+//  OnlySwift.swift
+//  CrossFrm
+//
+//  Created by Andreea Erdelyi on 22/02/2017.
+//
+//
+
+import Foundation
+
+public class OnlySwift {
+
+    public func lenght(ofText text: String) -> Int {
+
+        return text.characters.count
+    }
+}
---
+++ Sources/SwiftFromNSObject.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftFromNSObject.swift
+//  CrossFrm
+//
+//  Created by Andreea Erdelyi on 22/02/2017.
+//
+//
+
+import Foundation
+
+public class SwiftFromNSObject: NSObject {
+
+    public func lenght(ofText text: String) -> Int {
+
+        return text.characters.count
+    }
+
+}
